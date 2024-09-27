83016a9aae752a4c65df9bfb146206002f8bdc07
ASUS TUF Gaming F15 <Spartan.0637@outlook.com>
2024-09-27 11:02:20 -0600

    [ADD]- Creacion de Proyecto DTO

---
+++ DTO/CamionesDTO.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DTO
+{
+    public class CamionesDTO
+    {
+        public int Id_Camion { get; set; }
+
+        public string matricula { get; set; }
+
+        public int capacidad { get; set; }
+
+        public string tipo_camion { get; set; }
+
+        public string urlfoto { get; set; }
+
+        public string marca { get; set; }
+
+        public string modelo { get; set; }
+
+        public double kilometraje { get; set; }
+
+        public bool disponibilidad { get; set; }
+
+    }
+}
